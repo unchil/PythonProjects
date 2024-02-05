93dde90cb5d009d605fad679070c83b054a2b171
unchil <yeopo77@icloud.com>
2024-02-05 22:38:37 +0900

    a

--- seabornTest.py
+++ seabornTest.py
@@ -10,38 +10,36 @@ def load_data():
     df = sns.load_dataset("tips")
     return df
 
-def main():
-    st.title("streamlit with plotly")
 
-    tips= load_data()
+st.title("streamlit with plotly")
 
-        #데이터 가공
-    m_tips = tips.loc[tips['sex'] == 'Male']
-    f_tips = tips.loc[tips['sex'] == 'Female']
+tips= load_data()
 
-    #시각화 차트
-    fig= make_subplots(rows=1,
-                       cols=2,
-                       subplot_titles=('Male','Female'),
-                       shared_xaxes=True,
-                       shared_yaxes=True,
-                       x_title='Total Bill($)'
-                        )
-    fig.add_trace(go.Scatter(x=m_tips['total_bill'],y=m_tips['tip'],
-                             mode='markers'),row=1,col=1)
-    fig.add_trace(go.Scatter(x=f_tips['total_bill'],y=f_tips['tip'],
-                             mode='markers'),row=1,col=2)
-    
-    fig.update_yaxes(title_text="Tip($)",row=1,col=1)
-    fig.update_xaxes(range=[0,60])
-    fig.update_layout(showlegend=False)
-    
-    
+    #데이터 가공
+m_tips = tips.loc[tips['sex'] == 'Male']
+f_tips = tips.loc[tips['sex'] == 'Female']
 
-    st.plotly_chart(fig, use_container_width=True)
+#시각화 차트
+fig= make_subplots(rows=1,
+                   cols=2,
+                   subplot_titles=('Male','Female'),
+                   shared_xaxes=True,
+                   shared_yaxes=True,
+                   x_title='Total Bill($)'
+                    )
+fig.add_trace(go.Scatter(x=m_tips['total_bill'],y=m_tips['tip'],
+                         mode='markers'),row=1,col=1)
+fig.add_trace(go.Scatter(x=f_tips['total_bill'],y=f_tips['tip'],
+                         mode='markers'),row=1,col=2)
+
+fig.update_yaxes(title_text="Tip($)",row=1,col=1)
+fig.update_xaxes(range=[0,60])
+fig.update_layout(showlegend=False)
 
 
-if __name__ == "__main__":
-    main()
+
+st.plotly_chart(fig, use_container_width=True)
+
+
 
 
