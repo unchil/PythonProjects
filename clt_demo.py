95ff67dfed9c17686da96831d3f1f3783a9b6d94
UnChil Yeo <yeopo77@icloud.com>
2024-02-07 13:26:35 +0900

    a

--- clt_demo.py
+++ clt_demo.py
@@ -9,16 +9,16 @@ list_of_means = []
 for i in range(0, 1000):
     list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 
-fig, axes_list = plt.subplots(1, 2, figsize=(8,5))
+fig, axes_list = plt.subplots(2, 1)
 
 #fig1, ax1 = plt.subplots()
 
-axes_list[0].plot(list_of_means)
+axes_list[0][0].plot(list_of_means)
 #st.pyplot(fig1)
 
 #fig2, ax2 = plt.subplots()
 #axes_list[0][1] = plt.hist(np.ones(4))
-axes_list[1].plot(np.ones(4))
+axes_list[1][0].plot(np.ones(4))
 
 plt.show()
 st.pyplot(fig)
