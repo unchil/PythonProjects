a58d1bd2d84ef48ceb36b4108ad0d4c736c45b29
UnChil Yeo <yeopo77@icloud.com>
2024-02-07 13:51:58 +0900

    a

--- clt_demo.py
+++ clt_demo.py
@@ -5,7 +5,7 @@ import numpy as np
 perc_heads = st.number_input(label = 'Change of Coins Landing on Heads',
                              min_value= 0.0, max_value=1.0, value=.5 )
 
-binom_dist = np.random.binomial(1, perc_heads, 1000)
+binom_dist = np.random.binomial(1, .5, 1000)
 list_of_means = []
 
 for i in range(0, 1000):
@@ -13,7 +13,7 @@ for i in range(0, 1000):
 
 fig, axes_list = plt.subplots()
 
-axes_list.plot(list_of_means, range=[0,1])
+axes_list = plt.hist(list_of_means, range=[0,1])
 
 
 st.pyplot(fig)
