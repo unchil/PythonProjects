567b9b29570fe2076ae6c417b612c83343f0462f
UnChil Yeo <yeopo77@icloud.com>
2024-02-07 11:13:32 +0900

    a

--- plot_demo.py
+++ plot_demo.py
@@ -5,7 +5,7 @@ import numpy as np
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 
-for in in range(1, 101):
+for i in range(1, 101):
 	status_text.text("%i%% Complete" %i)
 	progress_bar.progress(i)
 	time.sleep(0.05)
