34f1d30a1ece7cedde631e017968d4e7dfe9fffb
UnChil Yeo <yeopo77@icloud.com>
2024-02-07 16:38:18 +0900

    a

--- penguins.py
+++ penguins.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
+import seaborn as sns
+
+penguins_df = pd.read_csv("../Streamlit-for-Data-Science/penguin_app/penguins.csv")
+
+st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
+selected_species = st.selectbox('What species would you likt to visualize?' , pd.unique(penguins_df['species']))
+selected_x_var = st.selectbox('What do you want the x variable to be?',
+                              ['pill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What do you want the y variable to be?',
+                               ['pill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+
 
 st.title("Palmer's Penguins")
-penguins_df = pd.read_csv("../Streamlit-for-Data-Science/penguin_app/penguins.csv")
+
 st.write(penguins_df.head())
