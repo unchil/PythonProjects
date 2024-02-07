a471379b58fac59f4d7a50d74a2f9eea0f3a87ab
UnChil Yeo <yeopo77@icloud.com>
2024-02-07 13:10:33 +0900

    a

--- clt_demo.py
+++ clt_demo.py
@@ -9,8 +9,10 @@ list_of_means = []
 for i in range(0, 1000):
     list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 
-plt.hist(list_of_means)
-st.pyplot()
+fig1, ax1 = plt.subplots()
+ax1 = plt.hist(list_of_means)
+st.pyplot(fig1)
 
-plt.hist(np.ones(4))
-st.pyplot()
+fig2, ax2 = plt.subplots()
+ax2 = plt.hist(np.ones(4))
+st.pyplot(fig2)
