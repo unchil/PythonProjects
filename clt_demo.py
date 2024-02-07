6afb26b0e7a9518a3ce6d04ce1481204112d1fbc
UnChil Yeo <yeopo77@icloud.com>
2024-02-07 12:45:40 +0900

    a

--- clt_demo.py
+++ clt_demo.py
@@ -1,5 +1,14 @@
+import matplotlib.pyplot as plt
 import streamlit as st
 import numpy as np
 
-binom_dist = np.random.binomial(1, .5, 100)
-st.write(np.mean(binom_dist))
+binom_dist = np.random.binomial(1, .5, 1000)
+
+list_of_means = []
+
+for i in range(0, 1000):
+    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
+
+fig, ax = plt.subplots()
+ax = plt.hist(list_of_means)
+st.pyplot(fig)
